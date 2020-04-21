538e20231307f53e9dd586d2f7be74b1c80380fa
VividerAphid <trever.kress@gmail.com>
2020-04-21 18:43:05 -0400

    Added fps counter support

--- src/com/mime/MysterySurvivalWorldGameV2/Display.java
+++ src/com/mime/MysterySurvivalWorldGameV2/Display.java
@@ -26,7 +26,7 @@ public class Display extends Canvas implements Runnable {
     private BufferedImage img;
     private int[] pixels;
     
-    public Display(){
+    public Display(){ 
         screen = new Screen(WIDTH, HEIGHT);
         img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
         pixels = ((DataBufferInt)img.getRaster().getDataBuffer()).getData();
@@ -57,9 +57,36 @@ public class Display extends Canvas implements Runnable {
     }
     
     public void run(){
+        int frames = 0;
+        double unprocessedSeconds = 0;
+        long previousTime = System.nanoTime();
+        double secondsPerTick = 1 / 60.0;
+        int tickCount = 0;
+        boolean ticked = false;
+        
         while(running){
-            tick();
+            long currentTime = System.nanoTime();
+            long passedTime = currentTime - previousTime;
+            previousTime = currentTime;
+            unprocessedSeconds += passedTime / 1000000000.0;
+            
+            while(unprocessedSeconds > secondsPerTick){
+                tick();
+                unprocessedSeconds -= secondsPerTick;
+                ticked = true;
+                tickCount++;
+                if(tickCount % 60 == 0){
+                    System.out.println(frames + " fps");
+                    previousTime += 1000;
+                    frames = 0;
+                }
+            }
+            if(ticked){
+                render();
+                frames++;
+            }
             render();
+            frames++;
         }
     }
     
--- src/com/mime/MysterySurvivalWorldGameV2/graphics/Screen.java
+++ src/com/mime/MysterySurvivalWorldGameV2/graphics/Screen.java
@@ -19,9 +19,9 @@ public class Screen extends Render{
         for(int i = 0; i< width*height; i++){
             pixels[i]=0;
         }
-        for(int i = 0; i <100; i++){          
-        int anim = (int) (Math.sin((System.currentTimeMillis() + i *5)  % 2000.0/2000* Math.PI * 2) * 200);
-        int anim2 = (int) (Math.cos((System.currentTimeMillis() + i *5) % 2000.0/2000* Math.PI * 2) * 200);
+        for(int i = 0; i <50; i++){          
+        int anim = (int) (Math.sin((System.currentTimeMillis() + i *8)  % 2000.0/2000* Math.PI * 2) * 200);
+        int anim2 = (int) (Math.cos((System.currentTimeMillis() + i *8) % 2000.0/2000* Math.PI * 2) * 200);
         draw(test, (width-256)/2 + anim, (height-256)/2 + anim2);
         }
     }
