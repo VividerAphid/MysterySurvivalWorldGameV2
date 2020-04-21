ea53dea387c34d89ba4021237be81a5e2cb4d8ac
VividerAphid <trever.kress@gmail.com>
2020-04-21 18:15:32 -0400

    Tested rendering animations

--- src/com/mime/MysterySurvivalWorldGameV2/graphics/Screen.java
+++ src/com/mime/MysterySurvivalWorldGameV2/graphics/Screen.java
@@ -16,6 +16,13 @@ public class Screen extends Render{
         }
     }
     public void render(){
-        draw(test, 0, 0);
+        for(int i = 0; i< width*height; i++){
+            pixels[i]=0;
+        }
+        for(int i = 0; i <100; i++){          
+        int anim = (int) (Math.sin(System.currentTimeMillis() % 2000.0/2000* Math.PI * 2) * 200);
+        int anim2 = (int) (Math.cos(System.currentTimeMillis() % 2000.0/2000* Math.PI * 2) * 200);
+        draw(test, (width-256)/2 + anim, (height-256)/2 + anim2);
+        }
     }
 }
